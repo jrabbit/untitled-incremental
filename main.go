b9ebbc80a17b4599005416366a1fea45350d0929
Jack Laxson <jackjrabbit+github@gmail.com>
2019-11-15 12:48:23 -0500

    game stuff

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"time"
-	"syscall/js"
 	"fmt"
+	"syscall/js"
+	"time"
 )
 
 type Level struct {
-	Name string
+	Name   string
 	Number int
 }
 
 type Scoreboard struct {
-	Teeth float64
-	Hats int
+	Teeth  float64
+	Hats   int
 	Levels []Level
 }
 
@@ -21,11 +21,10 @@ var scoreboard Scoreboard
 
 type processor func(time.Duration)
 
-
-func threadexperiment(d_time time.Duration){
+func threadexperiment(d_time time.Duration) {
 	// can we pass functions, can we call them sequentially?
 	//identity function
-	idfn := func(t time.Duration){scoreboard.Teeth = t.Seconds()}
+	idfn := func(t time.Duration) { scoreboard.Teeth = t.Seconds() }
 	var funcs []processor
 	funcs[0] = idfn
 	for _, f := range funcs {
@@ -33,22 +32,39 @@ func threadexperiment(d_time time.Duration){
 	}
 }
 
-func firstRun(){
-	js.Global().Get("localStorage").Call("setItem", "start_time", time.Now().Unix())
+func firstRun() {
+	cb := func() {
+		js.Global().Get("localStorage").Call("setItem", "start_time", time.Now().Unix())
+		fmt.Println("we started!!!")
+	}
+	query(".game-init").Call("addEventListener", "click", cb)
 }
 
+func query(qs string) js.Value {
+	return js.Global().Get("document").Call("querySelector", qs)
+}
+
+func blit() {
+	query("nav > h2.teeth").Call("textContent", fmt.Sprintf("%d teeth", scoreboard.Teeth))
+}
+
+func secondRun(d_time time.Duration) {
+	query("#game-area").Call("removeChild", query(".game-init"))
+}
 
 func main() {
 	scoreboard = Scoreboard{}
 	js_time := js.Global().Get("localStorage").Call("getItem", "start_time")
-	if js_time == js.Null(){
+	if js_time == js.Null() {
 		//user's first time
 		firstRun()
+	} else {
+		set_time, _ := time.Parse(time.UnixDate, js_time.String())
+		d_time := time.Now().Sub(set_time)
+		secondRun(d_time)
+		blit()
 	}
-	//set_time, _ := time.Parse(time.RFC3339, js_time)
 	fmt.Println(js_time)
-	//d_time := time.Now().Sub(set_time)
 	//fmt.Printf("time since game init: %d sec", d_time)
-	// g, _ := time.Parse("2009", "2006")
 	//threadexperiment(time.Now().Sub(g))
 }
